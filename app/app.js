d2e89aafea4f6f8f36dc1309ec8094fb3ac2df65
aponasi178cm <itokazunari5375@icloud.com>
2020-12-06 09:01:24 +0900

    新規登録APIの作成

--- app/app.js
+++ app/app.js
@@ -1,13 +1,19 @@
 // exporess(webフレームワーク)を利用する準備
 const express = require('express')
 const app = express()
+
 // sqliteを利用できるように設定する
 const sqlite3 = require('sqlite3')
 const dbPath = "app/db/database.sqlite3"
+const bodyParser = require('body-parser');
 
-const path = require('path')
 // publicディレクトリを静的ファイル群のルートディレクトリとして設定
+const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
+
+// リクエストのbodyを読み取る設定
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 
 // 全てのデータを取得するAPI
@@ -20,16 +26,16 @@ app.get('/api/v1/users', (req, res) => {
   db.close()
 })
 
-// idによって一部取得するAPI
-app.get('/api/v1/users/:id', (req, res) => {
-  // Connect database
-  const db = new sqlite3.Database(dbPath)
+// idごとに取得できるAPI
+app.get('/api/v1/users/:id', (req, res)=>{
+  const db = new sqlite3.Database(dbPath);
   const id = req.params.id
-  db.get(`SELECT * FROM users WHERE ${id}`, (err, rows) => {
+  db.get(`SELECT * FROM users WHERE id=${id}`, (err, rows)=>{
     res.json(rows)
   })
   db.close()
 })
+
 
 // Search users matching keyword
 app.get('/api/v1/search', (req, res) => {
@@ -42,8 +48,42 @@ app.get('/api/v1/search', (req, res) => {
   db.close()
 })
 
+// Create a new user
+app.post('/api/v1/users', async (req, res) => {
+  // Connect database
+    const db = new sqlite3.Database(dbPath)
+
+    const name = req.body.name
+    const profile = req.body.profile ? req.body.profile : ""
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
+
+    const run = async(sql) => {
+      return new Promise ((resolve, reject) => {
+        db.run(sql, (err) => {
+          if(err){
+            res.status(500).send(err);
+            return reject();
+          }else{
+            res.json({message: '新規登録成功しました'})
+            return resolve
+          }
+        })
+      })
+    }
+    
+    await run(
+        `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`
+      )
+    db.close()
+})
+  
+
+
+  
+
 // ポート番号の指定(環境変数を参照orポート番号3000の指定)
 const port = process.env.PORT || 3000;
 app.listen(port);
 // 成功でログとポート番号を表示
 console.log("Listen on port" + port);
+
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -1,25 +1,25 @@
 // 即時関数でモジュールかする
-
-
 const usersModule = (()=>{
   const BASE_URL = "http://localhost:3000/api/v1/users"
 
   return {
-    fetchAllUsers: async () => {
+    fetchAllUsers: async ()=>{
+      // GETメソットでAPIを叩く
       const res = await fetch(BASE_URL)
-      const users = await res.json()
+      const users = await res.json();
       let body = ""
-      for (let i=0; i < users.length; i++) {
+      for(let i=0; i<users.length; i++){
         const user = users[i]
-        body += `<tr>
-                        <td>${user.id}</td>
-                        <td>${user.name}</td>
-                        <td>${user.profile}</td>
-                        <td>${user.date_of_birth}</td>
-                        <td>${user.created_at}</td>
-                        <td>${user.updated_at}</td>        
-                      </tr>`
-        document.getElementById('users-list').innerHTML = body
+        body+= `<tr>
+                  <td>${user.id}</td>
+                  <td>${user.name}</td>
+                  <td>${user.profile}</td>
+                  <td>${user.date_of_birth}</td>
+                  <td>${user.created_at}</td>
+                  <td>${user.updated_at}</td>
+                </tr>`
+        document.getElementById("users-list").innerHTML = body
       }
-    }}
+    }
+    }
 })()
