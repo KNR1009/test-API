1b1cd4baca22846f42151619d396ee5d68416e51
aponasi178cm <itokazunari5375@icloud.com>
2020-12-09 22:18:57 +0900

    usersを新規で作成するAPIを叩く

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,10 +1,25 @@
 // エントリーポイント
 const indexModule = (()=>{
+  const path = window.location.pathname
+  
+  switch(path){
+    case '/':
+      // 検索ボタンをクリックしたときの挙動
+      document.getElementById('search-btn').addEventListener('click', () => {
+            return searchModule.searchUsers()
+          })
+      // userモジュールのfetchAllUsersメソットを呼び出す
+      return usersModule.fetchAllUsers()
+      break;
 
-  // 検索ボタンをクリックしたときの挙動
- document.getElementById('search-btn').addEventListener('click', () => {
-        return searchModule.searchUsers()
+    case '/create.html':
+      document.getElementById('save-btn').addEventListener('click', () => {
+        return usersModule.createUser()
       })
-  // userモジュールのfetchAllUsersメソットを呼び出す
-  return usersModule.fetchAllUsers()
+  }
+
+
+
+  // 新規作成ボタンをクリック
+  
 })()
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -2,6 +2,9 @@
 const usersModule = (()=>{
   const BASE_URL = "http://localhost:3000/api/v1/users"
 
+  const headers = new Headers()
+  headers.set("Content-Type", "application/json")
+
   return {
     fetchAllUsers: async ()=>{
       // GETメソットでAPIを叩く
@@ -20,6 +23,33 @@ const usersModule = (()=>{
                 </tr>`
         document.getElementById("users-list").innerHTML = body
       }
+    },
+    // 以下に新規登録APIを叩くためのメソットを作成する
+    createUser: async() => {
+      // 入力された値を取得する
+      const name = document.getElementById('name').value
+      const profile = document.getElementById('profile').value
+      const dateOfbirth = document.getElementById('date-of-birth').value
+
+      // リクエストbodyに代入して、bodypaserで変換してくれる
+      const body = {
+        name: name,
+        profile: profile,
+        date_ofc_birth:dateOfbirth
+      }
+
+      // POSTメソットを叩く
+       const res = await fetch(BASE_URL, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(body)
+      })
+
+      const resJson = await res.json()
+
+      // 成功した時にalertを出す(res.jsonの値)
+      alert(resJson.message)
+      window.location.href = "/"
     }
     }
 })()
